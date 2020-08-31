4f171f26f54c6571368620f5b9b1202e1948a73e
bakbledy93 <phim.kevin@gmail.com>
2020-08-31 19:12:59 +0200

    cart user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :cart_creation
+
+  def cart_creation
+    puts "hello"
+    Cart.create(
+      user_id: User.last.id
+    )
+  end
+
   
   has_one :cart
 
