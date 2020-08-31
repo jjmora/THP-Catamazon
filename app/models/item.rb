5fab8bd31a8e6f9b621dcd23af111df7820a7056
bakbledy93 <phim.kevin@gmail.com>
2020-08-31 14:39:23 +0200

    add validation on Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  
+  validates :title, 
+    presence: true,
+  validates :description,
+    presence: true,
+  validates :price,
+    presence: true,
+    numericality: true,
+
 end
