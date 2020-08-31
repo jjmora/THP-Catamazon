12baed78a9911bfffa38145c5ff9cf3a6f7cfcad
samielatri <sami.elatri@yahoo.com>
2020-08-31 21:35:57 +0200

    making root to new index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'page/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: 'page#index'
   root to: 'items#index'
   resources :items, only: [:index, :show]
 end
