22f50e7dab9ee00cf894ffa288fd5d897b9bdf21
DavidBeraud <dberaud@gmail.com>
2020-09-01 09:29:15 +0200

    css_ok

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor')
 Rails.application.config.assets.paths << Rails.root.join('lib')
-Rails.application.config.assets.paths << Rails.root.join('app')
+Rails.application.config.assets.paths << Rails.root.join('app', "assets", "img")
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
