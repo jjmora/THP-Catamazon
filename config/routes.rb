05715209e0f79592ff5016df6b2d79c818b51bc0
bakbledy93 <phim.kevin@gmail.com>
2020-09-01 20:35:36 +0200

    routes and stripe initializer

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :cart, only: [:create, :update, :new, :destroy, :show]
   resources :list_items, only: [:create, :update, :new, :destroy]
   resources :order, only: [:create, :update, :destroy]
+  resources :charges
 end
