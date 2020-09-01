c9eeb8eeac81841071d0cc643076a95f072ccb1a
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-01 11:17:06 +0200

    Controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  # get 'list_items/update
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'items#index'
   resources :items
+  resources :cart, only: [:update, :new, :destroy]
+  resources :list_items, only: [:update, :new, :destroy]
 end
