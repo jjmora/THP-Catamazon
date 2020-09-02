1eedef6dc675b2eda25951ad3f878add16d19c87
bakbledy93 <phim.kevin@gmail.com>
2020-09-02 14:11:41 +0200

    Sendgrid

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,15 @@
 class UserMailer < ApplicationMailer
-  default from: 'no-reply@monsite.fr'
+  default from: 'catmazon99@yopmail.com'
  
   def welcome_email(user)
-    mail(from: "hello@catmazon.fr", to: user.email,
-         subject: "Bienvenido on Catmazon")
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @user = user 
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'http://monsite.fr/login' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
 end
+
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -15,7 +14,8 @@ module CatAmazon
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.action_mailer.delivery_method = :mailjet_api
+    # config.action_mailer.delivery_method = :mailjet
   end
 end
+
 
