cfe3a97e5541de91f45c2af4cf592cca643e66ad
bakbledy93 <phim.kevin@gmail.com>
2020-09-02 15:01:15 +0200

    email when order validate

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,14 +2,19 @@ class UserMailer < ApplicationMailer
   default from: 'catmazon99@yopmail.com'
  
   def welcome_email(user)
-    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = user 
-
-    #on définit une variable @url qu'on utilisera dans la view d’e-mail
     @url  = 'http://monsite.fr/login' 
-
-    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+  end
+
+
+  def order_email(current_user)
+    puts "*"*150
+    p current_user.email
+    puts "*"*150
+    @user = current_user
+    @url  = 'http://monsite.fr/login' 
+    mail(to: @user.email, subject: 'Votre commande a été passé !') 
   end
 end
 
