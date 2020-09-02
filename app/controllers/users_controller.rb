77e6e917f5cab09e227354e218104b58da033fef
samielatri <sami.elatri@yahoo.com>
2020-09-02 14:24:25 +0200

    updated users show view for profile support and better structure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def show
     puts "User show controller"
-    @user = current_user
+    #@user = current_user
     # if !current_user
     #   redirect_to root
     # end
 
-    #@user = User.find(params[:id])
+    @user = User.find_by_id(params[:id])
   end
 end
