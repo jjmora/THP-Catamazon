f23985e4c38447442ad5b6952d52c684cc287630
bakbledy93 <phim.kevin@gmail.com>
2020-09-02 17:09:17 +0200

    Stripe update

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,15 +1,16 @@
 class ChargesController < ApplicationController
   @@price = 0
-
-  def new
-    @price = params[:price].to_f
-    @@price = @price
-  end
   
   def create
     # Amount in cents
-    @amount = @@price.to_i*100
-  puts @amount
+    @price = params[:price].to_f
+    puts "/"*150
+    puts @price
+    puts "/"*150
+    @amount = @price*100
+    puts "$"*150
+    puts @amount.to_i
+    puts "$"*150
     customer = Stripe::Customer.create({
       email: params[:stripeEmail],
       source: params[:stripeToken],
@@ -17,12 +18,12 @@ class ChargesController < ApplicationController
   
     charge = Stripe::Charge.create({
       customer: customer.id,
-      amount: @amount,
+      amount: @amount.to_i,
       description: 'Rails Stripe customer',
       currency: 'usd'
     })
 
-    @order = Order.create!(price: @@price, user_id: current_user.id )
+    @order = Order.create!(price: @price, user_id: current_user.id )
     @current_cart = Cart.where(user_id: current_user.id)
     @current_cart_items = @current_cart.first.items
 
@@ -34,6 +35,7 @@ class ChargesController < ApplicationController
     end
 
     redirect_to root_path
+    
     
     ListItem.where(cart_id: @current_cart.first.id).destroy_all
 
