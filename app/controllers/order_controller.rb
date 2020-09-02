7966a554eecef08d8b8e16da5bc879c3bd99a9ab
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-02 12:10:40 +0200

    order working in test mode

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -13,7 +13,13 @@ class OrderController < ApplicationController
   def create   
     @price = params[:price]
     @order = Order.create!(price: @price, user_id: current_user.id )
+    #On recupere les items du Cart
+    @current_cart = Cart.where(user_id: current_user.id)
+    @current_cart_items = @current_cart.first.items
 
+    @current_cart_items.each do |listorder|
+      ListOrder.create!(order_id: @order.id, item_id: listorder.id)
+    end
     redirect_to order_index_path
   end
 
