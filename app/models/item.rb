669325eb1aa6280c3feead93ecf7cf3d0fa9b7a0
samielatri <sami.elatri@yahoo.com>
2020-09-03 12:09:39 +0200

    edited model for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,10 @@ class Item < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: true
+  validates_attachment :image, presence: true,
+                       content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
+                       size: { less_than: 5.megabytes}
+  has_attached_file :image, styles: { medium: "320x240"}
 
   has_many :list_items
   has_many :carts, through: :list_items
